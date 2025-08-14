81691b38fd00ce0274a8147baaefb2ea081d404e
Satoru Takagi <satakagi@users.noreply.github.com>
2025-08-14 13:29:06 +0900

    Update i2cgps.ino

--- i2cgps.ino
+++ i2cgps.ino
@@ -13,69 +13,53 @@
  * calls those pins 27 and 28 and the Wire library just assumes it can use them.
  *
  */
+/*
+ * Yet Another I2C GPS firmware
+ *
+ * NeoGPSでGPSデータを解析し、I2C経由でデータを送信します。
+ * NeoSWSerialを使用して、ソフトウェアシリアル通信を行います。
+ */
 
 #include <Wire.h>
-#include <WireData.h>
+#include <WireData.h> // I2Cで構造体を送信するためのライブラリ
 
-// #define VERBOSE  // Uncomment this to print stuff to the UART
-#ifdef USE_NEO_SWSERIAL
+// ソフトウェアシリアルライブラリをインクルード
 #include <NeoSWSerial.h>
-#define DEBUG_PORT Serial
-#define GPS_PORT_NAME "NeoSWSerial(10,19)"
-NeoSWSerial gpsPort(10, 19);  // RX=10, TX=19 which is not connected
-#endif
-
-#include <GPSport.h>
 #include <NMEAGPS.h>
 
-const int i2c_addr = 0x58; // Because 'X' marks the spot.
+// GPSモジュールのボーレート
+const long GPS_BAUD_RATE = 9600;
 
+// PCのシリアルモニターのボーレート
+const long PC_BAUD_RATE = 57600;
+
+// I2Cアドレス
+const int i2c_addr = 0x58; // 'X' marks the spot.
+
+// GPSモジュールのRXピンとTXピンを設定
+// TXピン(19)は未使用
+NeoSWSerial gpsPort(10, 19); 
+
+// NeoGPSオブジェクトの宣言
+NMEAGPS gps;
+gps_fix fix;
+
+// I2Cで送信するデータ構造体
 typedef struct fix {
-  uint8_t fixlen; // first byte, so you can do a 1 byte i2c read to know the size of struct to receive
-  uint8_t status; // whatever NeoGPS says our fix status is
+  uint8_t fixlen; // 構造体のサイズ
+  uint8_t status;
   uint16_t year;
   uint8_t month, day, hour, minute, second;
   int32_t lat, lon;
   int32_t alt;
-  uint32_t speed; // milli-knots.
+  uint32_t speed;
 } Fix_t;
 
-static NMEAGPS  gps;
-static gps_fix  fix;
+// 送信するデータ構造体
 static Fix_t i2c_fix;
 
-void setup() {
-#ifdef VERBOSE
-  Serial.begin(57600);
-#endif // VERBOSE
-  gpsPort.begin(9600);
-  i2c_fix.fixlen = sizeof(i2c_fix);
-  Wire.begin(i2c_addr);
-  Wire.onRequest(i2c_position_request);
-  pinMode(LED_BUILTIN, OUTPUT);
-}
-
-unsigned long last_t = 0;
-void loop() {
-  while (gps.available( gpsPort )) {
-    fix = gps.read();
-    digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN)^1);
-
-#ifdef VERBOSE
-    Serial.print("Status: "); Serial.println(fix.status);
-    Serial.print("Latitude : "); Serial.println(fix.latitude(), 7);
-    Serial.print("Longitude: "); Serial.println(fix.longitude(), 7);
-    Serial.print("Altitude: "); Serial.println(fix.altitude());
-    Serial.print("Speed: "); Serial.println(fix.speed_mph());
-    // Serial.print("Sats: "); Serial.println(gps.sat_count);
-    Serial.print("Time:"); Serial.println((String)" " + (2000+fix.dateTime.year) + "/" + fix.dateTime.month + "/" + fix.dateTime.day + " " + fix.dateTime.hours + ":" + fix.dateTime.minutes + ":" + fix.dateTime.seconds);
-    Serial.println("");
-#endif // VERBOSE
-
-  }
-}
-
-void nmea2struct() {
+// I2Cリクエストハンドラ
+void i2c_position_request() {
   i2c_fix.status = fix.status;
   i2c_fix.lat = fix.latitudeL();
   i2c_fix.lon = fix.longitudeL();
@@ -87,10 +71,60 @@ void nmea2struct() {
   i2c_fix.hour = fix.dateTime.hours;
   i2c_fix.minute = fix.dateTime.minutes;
   i2c_fix.second = fix.dateTime.seconds;
+  
+  // I2C経由で構造体を送信
+  wireWriteData(i2c_fix);
 }
 
 
-void i2c_position_request() {
-  nmea2struct();
-  wireWriteData(i2c_fix);
+void setup() {
+  // PCとのシリアル通信を開始
+  Serial.begin(PC_BAUD_RATE);
+  Serial.println("Arduino Pro Mini has started.");
+  Serial.println("Waiting for GPS data...");
+  
+  // GPSモジュールとの通信を開始
+  gpsPort.begin(GPS_BAUD_RATE);
+  
+  // I2C通信を開始
+  i2c_fix.fixlen = sizeof(i2c_fix);
+  Wire.begin(i2c_addr);
+  Wire.onRequest(i2c_position_request);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop() {
+  // GPSポートにデータが届いていれば、
+  // NeoGPSでデータを解析する
+  if (gps.available(gpsPort)) {
+    // 解析済みのデータを取得
+    fix = gps.read();
+    
+    // LEDを点滅させて、正常に受信・解析されていることを示す
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    
+    // デバッグ用のシリアル出力
+    Serial.println("--------------------");
+    Serial.print("Status: ");
+    Serial.println(fix.status);
+    Serial.print("Latitude: ");
+    Serial.println(fix.latitude(), 7);
+    Serial.print("Longitude: ");
+    Serial.println(fix.longitude(), 7);
+    Serial.print("Date: ");
+    Serial.print(fix.dateTime.year);
+    Serial.print("/");
+    Serial.print(fix.dateTime.month);
+    Serial.print("/");
+    Serial.print(fix.dateTime.day);
+    Serial.print(" ");
+    Serial.print(fix.dateTime.hours);
+    Serial.print(":");
+    Serial.print(fix.dateTime.minutes);
+    Serial.print(":");
+    Serial.println(fix.dateTime.seconds);
+    Serial.println("--------------------");
+  }
 }
